2d76eb7e63d4ccc36513b48ca78097af619bc495
mesihg <mesihgrmay@yahoo.com>
2023-04-07 04:40:03 -0700

    bug fix

--- utils/redis.js
+++ utils/redis.js
@@ -1,38 +1,38 @@
 import {
-  createClient
+  createClient,
 } from 'redis';
 import {
-  promisify
+  promisify,
 } from 'util';
 
 class RedisClient {
-  constructor () {
+  constructor() {
     this.client = createClient();
     this.client.on('error', (error) => {
       console.log(`Redis client not connected to server: ${error}`);
     });
   }
 
-  isAlive () {
+  isAlive() {
     if (this.client.connected) {
       return true;
     }
     return false;
   }
 
-  async get (key) {
+  async get(key) {
     const redisGet = promisify(this.client.get).bind(this.client);
     const value = await redisGet(key);
     return value;
   }
 
-  async set (key, value, time) {
+  async set(key, value, time) {
     const redisSet = promisify(this.client.set).bind(this.client);
     await redisSet(key, value);
     await this.client.expire(key, time);
   }
 
-  async del (key) {
+  async del(key) {
     const redisDel = promisify(this.client.del).bind(this.client);
     await redisDel(key);
   }
