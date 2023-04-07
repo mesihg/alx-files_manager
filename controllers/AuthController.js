e9126e8525ed9daa833030ada63c5d6752bf4194
mesihg <mesihgrmay@yahoo.com>
2023-04-07 12:27:45 -0700

    bug fix for AuthController

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -8,32 +8,20 @@ import redisClient from '../utils/redis';
 class AuthController {
   static async getConnect(req, res) {
     const authToken = req.header('Authorization');
-    if (!authToken) {
-      res.status(401).json({ error: 'Unauthorized' });
-      return;
-    }
-
     const authTokenData = Buffer.from(authToken, 'base64').toString();
     const [email, password] = authTokenData.split(':');
 
-    const hashedPassword = sha1(password);
-    const users = dbClient.db.collection('users');
-    users.findOne({
-      email,
-      password: hashedPassword,
-    }, async (err, user) => {
-      if (user) {
-        const token = uuidv4();
-        const key = `auth_${token}`;
-        await redisClient.set(key, user._id.toString(), 60 * 60 * 24);
-        res.status(200).json({
-          token,
-        });
-      } if (err) {
-        res.status(401).json({
-          error: 'Unauthorized',
-        });
-      }
+    const userQuery = { email, password: sha1(password) };
+    const user = await dbClient.db.collection('users').findOne(userQuery);
+    if (!user) {
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
+    const token = uuidv4();
+    const key = `auth_${token}`;
+    await redisClient.set(key, user._id.toString(), 60 * 60 * 24);
+    res.status(200).json({
+      token,
     });
   }
 
