7bf6dd4cef5ccf988ddc9f5a0db59b723e85bb7b
mesihg <mesihgrmay@yahoo.com>
2023-04-07 04:43:46 -0700

    bug fix for db

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,5 @@
 import {
-  MongoClient
+  MongoClient,
 } from 'mongodb';
 
 const HOST = process.env.DB_HOST || 'localhost';
@@ -9,10 +9,10 @@ const DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
-  constructor () {
+  constructor() {
     this.client = new MongoClient(url, {
       useUnifiedTopology: true,
-      useNewUrlParser: true
+      useNewUrlParser: true,
     });
     this.client.connect().then(() => {
       this.db = this.client.db(`${DATABASE}`);
@@ -21,17 +21,17 @@ class DBClient {
     });
   }
 
-  isAlive () {
+  isAlive() {
     return this.client.isConnected();
   }
 
-  async nbUsers () {
+  async nbUsers() {
     const users = this.db.collection('users');
     const usersNum = await users.countDocuments();
     return usersNum;
   }
 
-  async nbFiles () {
+  async nbFiles() {
     const files = this.db.collection('files');
     const filesNum = await files.countDocuments();
     return filesNum;
